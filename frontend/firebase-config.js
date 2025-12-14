f1a127aaab9a89f931b83d619fc6e44009ffea7d
Delyplott <danielsalas.gallardo@protonmail.com>
2025-12-14 15:11:58 -0300

    Update firebase-config.js

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -5,11 +5,8 @@ window.FIREBASE_CONFIG = {
   storageBucket: "delyplott-web.firebasestorage.app",
   messagingSenderId: "1053240217320",
   appId: "1:1053240217320:web:e2116890e66e0b5fbb035d",
-  measurementId: "G-2KLXLY1MF7 // opcional
+  measurementId: "G-2KLXLY1MF7" // opcional (puedes borrar esta línea si quieres)
 };
 
-// Tu Apps Script Web App (Drive Bridge)
 window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxN8Tu3P7nGh_6p_QDWEYbuecn800YtQJZZGYssZq1FIr04QbSrDCeO9PIUjVpVfKJpBg/exec";
-
-// Ajustes
 window.DELY_SETTINGS = { MAX_FILE_MB: 20 };

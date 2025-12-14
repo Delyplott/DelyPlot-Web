ad04e6bf4551afd60fc6ff8cac9764b9b9088211
Delyplott <danielsalas.gallardo@protonmail.com>
2025-12-14 14:42:03 -0300

    Update firebase-config.js

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,15 +1,15 @@
-// Firebase web config (Settings -> General -> Your apps -> Web app)
 window.FIREBASE_CONFIG = {
-  apiKey: "XXX",
-  authDomain: "XXX.firebaseapp.com",
-  projectId: "XXX",
-  appId: "XXX"
+  apiKey: "AIzaSyCo-UA6YdvqHaI0cNgqoBtkhj3ZpVhJiL0",
+  authDomain: "delyplott-web.firebaseapp.com",
+  projectId: "delyplott-web",
+  storageBucket: "delyplott-web.firebasestorage.app",
+  messagingSenderId: "1053240217320",
+  appId: "1:1053240217320:web:e2116890e66e0b5fbb035d",
+  measurementId: "G-2KLXLY1MF7 // opcional
 };
 
-// Apps Script Web App URL (Deploy -> Web app)
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/XXXX/exec";
+// Tu Apps Script Web App (Drive Bridge)
+window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxN8Tu3P7nGh_6p_QDWEYbuecn800YtQJZZGYssZq1FIr04QbSrDCeO9PIUjVpVfKJpBg/exec";
 
-// Límites prácticos del bridge (Apps Script)
-window.DELY_SETTINGS = {
-  MAX_FILE_MB: 20
-};
+// Ajustes
+window.DELY_SETTINGS = { MAX_FILE_MB: 20 };
